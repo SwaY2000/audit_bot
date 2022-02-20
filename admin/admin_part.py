d55521673f8047039068dc1c82054f8e407833da
Danil <dan.frais2000@gmail.com>
2022-02-20 23:09:21 +0200

    Add verification admin in admin part function

--- admin/admin_part.py
+++ admin/admin_part.py
@@ -2,34 +2,39 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
 
-from help_file import dp, bot
+from help.help_file import dp, bot
 from keyboards.admin_kb import download_inline
 from pandas_test.excel_method import create_list_excel
 
-ADMIN_ID = '387353019'
+ADMIN_ID = ['387353019', '']
 
 class FSMAdmin(StatesGroup):
     day_admin = State()
 
 async def download(message: types.Message):
     """This is method send document for admin"""
-    await message.reply_document(open('excel_py.xlsx', 'rb'))
+    if message.chat.id in ADMIN_ID:
+        await message.reply_document(open('excel_py.xlsx', 'rb'))
+    else:
+        await message.reply('Эта функция Вам недоступна')
     return
 
 async def create_new_document(message: types.Message):
     """This is method download document and create new document"""
-    inline_create = download_inline()
-    try:
-        await message.reply_document(open('excel_py.xlsx', 'rb'))
-    except:
-        pass
-    await message.reply('Выберите сколько дней будет в документе', reply_markup=inline_create)
-    await FSMAdmin.next()
+    if message.chat.id in ADMIN_ID:
+        inline_create = download_inline()
+        try:
+            await message.reply_document(open('excel_py.xlsx', 'rb'))
+        except:
+            pass
+        await message.reply('Выберите сколько дней будет в документе', reply_markup=inline_create)
+        await FSMAdmin.next()
+    else:
+        await message.reply('Эта функция Вам недоступна')
+    return
 
 async def inline_create(callback_query: types.CallbackQuery, state: FSMContext):
     create_list_excel(callback_query.data)
-    # async with state.proxy() as cell:
-    #     cell['day'] = callback_query.data
     await state.finish()
     await callback_query.message.edit_text('Создан новый документ')
     return
